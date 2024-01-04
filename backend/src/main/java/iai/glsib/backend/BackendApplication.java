5b34dbf9946f5dd3cdbdbe7c26d34efb6c7794eb
Raikirimaru <amemawuli8@gmail.com>
2024-01-04 22:14:20 +0000

    create the entities for api

--- backend/src/main/java/iai/glsib/backend/BackendApplication.java
+++ backend/src/main/java/iai/glsib/backend/BackendApplication.java
@@ -1,7 +1,24 @@
 package iai.glsib.backend;
 
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import iai.glsib.backend.entities.AccountOperation;
+import iai.glsib.backend.entities.CurrentAccount;
+import iai.glsib.backend.entities.Customer;
+import iai.glsib.backend.entities.SavingAccount;
+import iai.glsib.backend.enums.AccountStatus;
+import iai.glsib.backend.enums.OperationType;
+import iai.glsib.backend.repositories.AccountOperationRepository;
+import iai.glsib.backend.repositories.BankAccountRepository;
+import iai.glsib.backend.repositories.CustomerRepository;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +26,52 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	/* @Bean
+	CommandLineRunner start(
+		CustomerRepository customerRepository,
+		BankAccountRepository bankAccountRepository,
+		AccountOperationRepository accountOperationRepository
+	) {
+		return args -> {
+			Stream.of("Hassan", "Yassine", "Aicha").forEach( name -> {
+				Customer customer = new Customer();
+				customer.setName(name);
+				customer.setEmail(name + "@gmail.com");
+				customerRepository.save(customer);
+			});
+
+			customerRepository.findAll().forEach( c -> {
+				CurrentAccount currentAccount = new CurrentAccount();
+				currentAccount.setId(UUID.randomUUID().toString());
+				currentAccount.setBalance(Math.random()*90000);
+				currentAccount.setCreatedAt(new Date());
+				currentAccount.setStatus(AccountStatus.CREATED);
+				currentAccount.setCustomer(c);
+				currentAccount.setOverDraft(9058);
+				bankAccountRepository.save(currentAccount);
+
+				SavingAccount savingAccount = new SavingAccount();
+				savingAccount.setId(UUID.randomUUID().toString());
+				savingAccount.setBalance(Math.random()*90000);
+				savingAccount.setCreatedAt(new Date());
+				savingAccount.setStatus(AccountStatus.CREATED);
+				savingAccount.setCustomer(c);
+				savingAccount.setInterestRate(5.5);
+				bankAccountRepository.save(savingAccount);
+			});
+
+			bankAccountRepository.findAll().forEach( acc -> {
+				for (int i = 0; i < 10; i++) {
+					AccountOperation accountOperation = new AccountOperation();
+					accountOperation.setOperationDate(new Date());
+					accountOperation.setAmount(Math.random() * 12000);
+					accountOperation.setType(Math.random() > 0.5 ? OperationType.DEBIT : OperationType.CREDIT);
+					accountOperation.setBankAccount(acc);
+					accountOperationRepository.save(accountOperation);
+				}
+			});
+		};
+	} */
 
 }
